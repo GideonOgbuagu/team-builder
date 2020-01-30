ec70dcfab7d6b8ea8dd890a32b02c25c0cfeddcc
Gideon <reachgiddy@gmail.com>
2020-01-30 02:24:37 -0500

    created code structure for the Team.js

--- my-app/src/components/Team.js
+++ my-app/src/components/Team.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 function Teams(props) {
     return (
-   
+    <div>
+        {props.team.map(member => {
+            <div className="card-container">
+                <h3>{memberName}</h3>
+                <p>{email}</p>
+                <p>{role}</p>
+            </div>
+        })}   
+    </div>
     )
 }
 
