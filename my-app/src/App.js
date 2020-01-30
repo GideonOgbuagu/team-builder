ec465a10450aaef3863d3725e6f298894b26eed9
Gideon <reachgiddy@gmail.com>
2020-01-30 02:23:03 -0500

    completed code for app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,19 +4,30 @@ import Team from './components/Team';
 import './App.css';
 
 function App() {
-  const [teams, setTeams] = useState({
-      name: 'John Smith',
+  const [team, setTeam] = useState([
+    {
+      memberName: 'John Smith',
       email: 'johnsmith@gmail.com',
       role: 'Frontend Engineer'
-  })
+    }
+  ]);
 
-  
+// Here we create a template or function 
+// for adding new team members
+  function addTeamMember(teamMember) {
+    const newTeamMember = {
+      memberName: teamMember.memberName,
+      email: teamMember.email,
+      role: teamMember.role
+    }
+    setTeam({...team, newTeamMember});
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <TeamForm />
-        <Team />
+        <TeamForm addTeamMember={addTeamMember}/>
+        <Team team={team}/>
       </header>
     </div>
   );
--- my-app/src/components/TeamForm.js
+++ my-app/src/components/TeamForm.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState} from 'react';
+
 
 function TeamForm() {
+const [teamMember, setTeamMember] = useState({
+    memberName: '',
+    email: '',
+    role: ''
+})
 
+const handleInputChange = e => {
+    
+}
+
+    return (
+        <form>
+            <label hmtlFor="">Name:</label>
+            <input type="text" id="" name="memberName" value={teamMember.memberName}/>
+            <label hmtlFor="">Name:</label>
+            <input type="email" id="" name="email" value={teamMember.email}/>
+            <label hmtlFor="">Name:</label>
+            <input type="text" id="" name="role" value={teamMember.role}/>
+
+        </form>
+    )
 }
 
 export default TeamForm;
